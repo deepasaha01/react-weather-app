56f2422243ea66a59b42acc6bb3f85c602e6b295
deepasaha01 <90994589+deepasaha01@users.noreply.github.com>
2022-02-11 13:06:34 +0900

    Added footer

--- src/App.js
+++ src/App.js
@@ -2,5 +2,20 @@ import React from "react";
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      <h1>Weather App</h1>
+      <footer>
+        This project was coded by Deepa Saha and is{" "}
+        <a
+          href="https://github.com/deepasaha01/react-weather-app"
+          target="_blank"
+          rel="noreferrer"
+        >
+          open-sourced
+        </a>{" "}
+        on GitHub.
+      </footer>
+    </div>
+  );
 }
