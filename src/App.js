b21c92fac9600eb6576228da5f9f92fcf8be29b9
deepasaha01 <90994589+deepasaha01@users.noreply.github.com>
2022-02-12 11:08:26 +0900

    Added bootstrap

--- src/App.js
+++ src/App.js
@@ -1,21 +1,24 @@
 import React from "react";
+import "bootstrap/dist/css/bootstrap.css";
 import "./App.css";
 
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <footer>
-        This project was coded by Deepa Saha and is{" "}
-        <a
-          href="https://github.com/deepasaha01/react-weather-app"
-          target="_blank"
-          rel="noreferrer"
-        >
-          open-sourced
-        </a>{" "}
-        on GitHub.
-      </footer>
+      <div className="container">
+        <h1>Weather App</h1>
+        <footer>
+          This project was coded by Deepa Saha and is{" "}
+          <a
+            href="https://github.com/deepasaha01/react-weather-app"
+            target="_blank"
+            rel="noreferrer"
+          >
+            open-sourced
+          </a>{" "}
+          on GitHub.
+        </footer>
+      </div>
     </div>
   );
 }
